2bb34de991042b309ce18af733fefdb9c9c28075
The Realist <nitin.armstrong@gmail.com>
2019-12-05 18:17:06 -0500

    reverted back to previous save state in attempt to fix missing tile issue

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 """
 Platformer Game
 """
-import wave
-import threading
+
 import sys
 import os
-import winsound
+
 import arcade
 import pyglet
 
@@ -37,6 +36,10 @@ INSTRUCTIONS = 1
 GAME_RUNNING = 2
 GAME_OVER = 3
 
+# Player Start
+PLAYER_START_X = 16
+PLAYER_START_Y = 16
+
 
 class MyGame(arcade.Window):
     """
@@ -44,90 +47,143 @@ class MyGame(arcade.Window):
     """
 
     def __init__(self):
+
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        # These are 'lists' that keep track of our sprites. Each sprite should
+        # go into a list.
         self.coin_list = None
         self.wall_list = None
+        self.foreground_list = None
+        self.background_list = None
+        self.dont_touch_list = None
         self.player_list = None
+
+        # Separate variable that holds the player sprite
         self.player_sprite = None
 
-        file_path = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(file_path)
-
-        arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+        # Our physics engine
+        self.physics_engine = None
 
         # Used to keep track of our scrolling
         self.view_bottom = 0
         self.view_left = 0
 
-        # Load the sounds
-        self.get_coin_sound = arcade.load_sound("sounds/coin.wav")
-        self.jump_sound = arcade.load_sound("sounds/jump.wav")
-        self.hurt_sound = arcade.load_sound("sounds/hurt.wav")
-        self.theme_music = arcade.load_sound("sounds/overworld_music.wav")
-
-        # score count
+        # Keep track of the score
         self.score = 0
 
-        # Right edge of the map
-        self.edge_of_map = 0
-
-    def setup(self):
-        """ Set up the game here. Call this function to restart the game. """
-        self.coin_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList()
-        self.player_list = arcade.SpriteList()
-
-        # Score Setup Here
-        self.score = 0
+        # Where is the right edge of the map?
+        self.end_of_map = 0
 
         # Level
         self.level = 1
 
-        # Player Setup here
-        self.player_sprite = arcade.Sprite("images/characters/player_standing.png", CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 64
+        # Load sounds
+        self.collect_coin_sound = arcade.load_sound("sounds/coin.wav")
+        self.jump_sound = arcade.load_sound("sounds/jump.wav")
+        self.game_over = arcade.load_sound("sounds/hurt.wav")
+
+    def setup(self, level):
+        """ Set up the game here. Call this function to restart the game. """
+
+        # Used to keep track of our scrolling
+        self.view_bottom = 0
+        self.view_left = 0
+
+        # Keep track of the score
+        self.score = 0
+
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.foreground_list = arcade.SpriteList()
+        self.background_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Set up the player, specifically placing it at these coordinates.
+        self.player_sprite = arcade.Sprite("images/characters/player_standing.png",
+                                           CHARACTER_SCALING)
+        self.player_sprite.center_x = PLAYER_START_X
+        self.player_sprite.center_y = PLAYER_START_Y
         self.player_list.append(self.player_sprite)
 
         # --- Load in a map from the tiled editor ---
 
-        # Name of map file to load
-        map_name = "map.tmx"
         # Name of the layer in the file that has our platforms/walls
         platforms_layer_name = 'Platform'
         # Name of the layer that has items for pick-up
         coins_layer_name = 'Coins'
+        # Name of the layer that has items for foreground
+        foreground_layer_name = 'Foreground'
         # Name of the layer that has items for background
         background_layer_name = 'Background'
         # Name of the layer that has items we shouldn't touch
         dont_touch_layer_name = "Don't Touch"
 
+        # Map name
+        map_name = f"map_level_{level}.tmx"
+
         # Read in the tiled map
         my_map = arcade.tilemap.read_tmx(map_name)
 
-        # -- Platforms
-        self.wall_list = arcade.tilemap.process_layer(my_map, platforms_layer_name, TILE_SCALING)
+        # Calculate the right edge of the my_map in pixels
+        self.end_of_map = my_map.map_size.width * GRID_PIXEL_SIZE
 
-        # -- Coins
-        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, TILE_SCALING)
-
-        # Background
+        # -- Background
         self.background_list = arcade.tilemap.process_layer(my_map,
                                                             background_layer_name,
                                                             TILE_SCALING)
+
+        # -- Foreground
+        self.foreground_list = arcade.tilemap.process_layer(my_map,
+                                                            foreground_layer_name,
+                                                            TILE_SCALING)
+
+        # -- Platforms
+        self.wall_list = arcade.tilemap.process_layer(my_map,
+                                                      platforms_layer_name,
+                                                      TILE_SCALING)
+
+        # -- Coins
+        self.coin_list = arcade.tilemap.process_layer(my_map,
+                                                      coins_layer_name,
+                                                      TILE_SCALING)
+
         # -- Don't Touch Layer
         self.dont_touch_list = arcade.tilemap.process_layer(my_map,
                                                             dont_touch_layer_name,
                                                             TILE_SCALING)
+
         # --- Other stuff
         # Set the background color
         if my_map.background_color:
             arcade.set_background_color(my_map.background_color)
-        # Play the theme music on loop
 
         # Create the 'physics engine'
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                                             self.wall_list,
+                                                             GRAVITY)
+
+    def on_draw(self):
+        """ Render the screen. """
+
+        # Clear the screen to the background color
+        arcade.start_render()
+
+        # Draw our sprites
+        self.wall_list.draw()
+        self.background_list.draw()
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.dont_touch_list.draw()
+        self.player_list.draw()
+        self.foreground_list.draw()
+
+        # Draw our score on the screen, scrolling it with the viewport
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom,
+                         arcade.csscolor.BLACK, 18)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -155,6 +211,7 @@ class MyGame(arcade.Window):
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
         self.physics_engine.update()
+
         # See if we hit any coins
         coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                              self.coin_list)
@@ -167,47 +224,10 @@ class MyGame(arcade.Window):
             arcade.play_sound(self.collect_coin_sound)
             # Add one to the score
             self.score += 1
-        # --- Manage Scrolling ---
 
         # Track if we need to change the viewport
+        changed_viewport = False
 
-        changed = False
-
-        # Scroll left
-        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
-        if self.player_sprite.left < left_boundary:
-            self.view_left -= left_boundary - self.player_sprite.left
-            changed = True
-
-        # Scroll right
-        right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
-        if self.player_sprite.right > right_boundary:
-            self.view_left += self.player_sprite.right - right_boundary
-            changed = True
-
-        # Scroll up
-        top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
-        if self.player_sprite.top > top_boundary:
-            self.view_bottom += self.player_sprite.top - top_boundary
-            changed = True
-
-        # Scroll down
-        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
-        if self.player_sprite.bottom < bottom_boundary:
-            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
-            changed = True
-
-        if changed:
-            # Only scroll to integers. Otherwise we end up with pixels that
-            # don't line up on the screen
-            self.view_bottom = int(self.view_bottom)
-            self.view_left = int(self.view_left)
-
-            # Do the scrolling
-            arcade.set_viewport(self.view_left,
-                                SCREEN_WIDTH + self.view_left,
-                                self.view_bottom,
-                                SCREEN_HEIGHT + self.view_bottom)
         # Did the player fall off the map?
         if self.player_sprite.center_y < -100:
             self.player_sprite.center_x = PLAYER_START_X
@@ -218,6 +238,7 @@ class MyGame(arcade.Window):
             self.view_bottom = 0
             changed_viewport = True
             arcade.play_sound(self.game_over)
+
         # Did the player touch something they should not?
         if arcade.check_for_collision_with_list(self.player_sprite,
                                                 self.dont_touch_list):
@@ -232,25 +253,62 @@ class MyGame(arcade.Window):
             changed_viewport = True
             arcade.play_sound(self.game_over)
 
-    def on_draw(self):
-        """ Render the screen. """
-        # Clear the Screen
-        arcade.start_render()
+        # See if the user got to the end of the level
+        if self.player_sprite.center_x >= self.end_of_map:
+            # Advance to the next level
+            self.level += 1
 
-        # Draw Sprites
-        self.wall_list.draw()
-        self.coin_list.draw()
-        self.player_list.draw()
+            # Load the next level
+            self.setup(self.level)
 
-        # Draw the amount of Lives the player has
-        score_text = f"Score: {self.score}"
-        arcade.draw_text(score_text, 10 + self.view_left, 600 + self.view_bottom, arcade.csscolor.WHITE, 18)
+            # Set the camera to the start
+            self.view_left = 0
+            self.view_bottom = 0
+            changed_viewport = True
+
+        # --- Manage Scrolling ---
+
+        # Scroll left
+        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
+        if self.player_sprite.left < left_boundary:
+            self.view_left -= left_boundary - self.player_sprite.left
+            changed_viewport = True
+
+        # Scroll right
+        right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
+        if self.player_sprite.right > right_boundary:
+            self.view_left += self.player_sprite.right - right_boundary
+            changed_viewport = True
+
+        # Scroll up
+        top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
+        if self.player_sprite.top > top_boundary:
+            self.view_bottom += self.player_sprite.top - top_boundary
+            changed_viewport = True
+
+        # Scroll down
+        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
+        if self.player_sprite.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+            changed_viewport = True
+
+        if changed_viewport:
+            # Only scroll to integers. Otherwise we end up with pixels that
+            # don't line up on the screen
+            self.view_bottom = int(self.view_bottom)
+            self.view_left = int(self.view_left)
+
+            # Do the scrolling
+            arcade.set_viewport(self.view_left,
+                                SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                SCREEN_HEIGHT + self.view_bottom)
 
 
 def main():
     """ Main method """
     window = MyGame()
-    window.setup()
+    window.setup(window.level)
     arcade.run()
 
 
